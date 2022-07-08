686129d6cbd0e6ded33d3cdb4dbe91c2fe5e7933
Lucas Barbosa <barbosa.lucash@gmail.com>
2022-07-08 16:09:33 -0300

    feat: implement create user usecase

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,18 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+  name: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+  admin = false;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
 }
 
 export { User };
